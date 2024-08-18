54a2fe325b64ba94a88b6d2ffa6e888ca847662f
cubercubegit <102414081+cubercubegit@users.noreply.github.com>
2024-08-18 01:05:50 +0100

    Add button click event listeners to save first operand value

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello world")
+let valueOne = 1;
+
+function setValueOne(){
+    valueOne = this.textContent;
+    console.log(valueOne);
+}
+
+let operatorSelection = 1;
+
+const allNumButtons = document.getElementsByClassName("number");
+
+for (i=0 ; i<allNumButtons.length ; i++){
+    allNumButtons[i].addEventListener("click", setValueOne);
+}
