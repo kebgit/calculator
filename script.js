3958d6105dfc61ae0b61df9a25bd1a71b15358c3
cubercubegit <102414081+cubercubegit@users.noreply.github.com>
2024-08-18 01:10:46 +0100

    Reorganized code

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
+const allNumButtons = document.getElementsByClassName("number");
+
+for (i=0 ; i<allNumButtons.length ; i++){
+    allNumButtons[i].addEventListener("click", setValueOne);
+}
+
 let valueOne = 1;
+let savedValue = false;
+let operatorSelection = 0;
 
 function setValueOne(){
     valueOne = this.textContent;
     console.log(valueOne);
 }
 
-let operatorSelection = 1;
 
-const allNumButtons = document.getElementsByClassName("number");
 
-for (i=0 ; i<allNumButtons.length ; i++){
-    allNumButtons[i].addEventListener("click", setValueOne);
-}
+
+
+
+
+// start
+// click number
+// save valueOne
+// Click operator
+// save operatorValue
+// Click number
+// save ValueTwo
+// click Equals sign
+// Update display to result, save result as valueOne
